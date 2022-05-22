2ba68e3fc6e5fcb2a74ada86641a0b06a8ffc7e4
정석호 <seokho.jeong@toss.im>
2022-05-22 21:54:14 +0900

    fix: header를 가운데 정렬

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,5 @@
 import Page from "@divops/component-page";
+import { css } from "@emotion/react";
 import Header from "next/head";
 import { Calendar } from "../components/calendar";
 import { pageStyle } from "../styles/page";
@@ -7,7 +8,14 @@ export default function HomePage() {
   const now = new Date();
 
   return (
-    <Page title={"내 칼로리"} css={pageStyle} header={Header}>
+    <Page
+      title={"내 칼로리"}
+      css={css`
+        ${pageStyle}
+        text-align: center;
+      `}
+      header={Header}
+    >
       <Calendar year={now.getFullYear()} month={now.getMonth() + 1} />
     </Page>
   );
