80e7ffcc1ce641b268fd5c4a26106d5d5bb6d468
seokho-jeong <seokho.jeong@toss.im>
2022-06-25 12:45:35 +0900

    fix: api entry 수정

--- src/services/calories.ts
+++ src/services/calories.ts
@@ -1,12 +1,27 @@
 import { get, post } from "./api";
 
+function getBaseURL() {
+  if (typeof window === "undefined") {
+    return "https://calories-log.net/api";
+  }
+
+  if (window.location.hostname === "life.creco.me") {
+    return "https://life.creco.me/calories/api";
+  }
+
+  if (window.location.hostname === "calories-log.net") {
+    return "https://calories-log.net/api";
+  }
+
+  return "http://localhost:3000/api";
+}
 export async function updateCalories({ data }: { data: any }) {
-  const resopnse = await post("/api/calories", { data });
+  const resopnse = await post(`${getBaseURL()}/calories`, { data });
   return resopnse.data;
 }
 
 export async function fetchCalories() {
-  const resopnse = await get("/api/calories");
+  const resopnse = await get(`${getBaseURL()}/calories`);
   const { data } = resopnse.data;
   return data;
 }
