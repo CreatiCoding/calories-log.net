e2783f5774a3e41d6e7949e3370dd4cfd5888d61
seokho-jeong <seokho.jeong@toss.im>
2022-06-25 13:04:32 +0900

    fix: api

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,5 +1,21 @@
 import { post } from "./api";
 
+function getBaseURL() {
+  if (typeof window === "undefined") {
+    return "https://calories-log.net/api";
+  }
+
+  if (window.location.hostname === "life.creco.me") {
+    return "https://life.creco.me/calories/api";
+  }
+
+  if (window.location.hostname === "calories-log.net") {
+    return "https://calories-log.net/api";
+  }
+
+  return "http://localhost:3000/api";
+}
+
 export async function loginKakao({ code }: { code: string }) {
-  return await post("/api/kakao/login", { code });
+  return await post(`${getBaseURL()}/kakao/login`, { code });
 }
