f7c808286e8d15ba5d201addf947f39fe52d1f93
seokho-jeong <seokho.jeong@toss.im>
2022-06-25 13:27:18 +0900

    fix: router

--- src/pages/KakaoLoginPage.tsx
+++ src/pages/KakaoLoginPage.tsx
@@ -2,6 +2,22 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { loginKakao } from "../services/user";
 
+function path(path: string) {
+  if (typeof window === "undefined") {
+    return path;
+  }
+
+  if (window.location.hostname === "life.creco.me") {
+    return `/calories${path}`;
+  }
+
+  if (window.location.hostname === "calories-log.net") {
+    return path;
+  }
+
+  return path;
+}
+
 export default function KakaoLoginPage() {
   const router = useRouter();
 
@@ -14,18 +30,18 @@ export default function KakaoLoginPage() {
 
     if (code == null || typeof code !== "string") {
       alert("인가 코드가 없습니다. 재로그인 부탁해요 🙏");
-      router.push("/");
+      router.push(path("/"));
       return;
     }
 
     (async () => {
       const { data } = await loginKakao({ code });
       if (data.email != null) {
-        router.push(`/?email=${data.email}`);
+        router.push(path(`/?email=${data.email}`));
         return;
       }
 
-      router.push("/");
+      router.push(path("/"));
     })();
   }, [router]);
 
